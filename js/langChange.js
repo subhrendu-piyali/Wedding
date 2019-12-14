5de5d10835e3590a183918917ae35deef0ca3c08
subhrendu@iitg.ernet.in <subhrendu@iitg.ernet.in>
2019-12-14 15:11:22 +0530

    modified:   about.html 	modified:   css/about.css 	copied:     css/about.css -> css/common.css 	modified:   css/index.css 	modified:   css/venue.css 	deleted:    dualLingo.html 	modified:   index.html 	new file:   js/langChange.js 	deleted:    subho.html 	modified:   venue.html

---
+++ js/langChange.js
@@ -0,0 +1,22 @@
+	      function LangBn(){
+	      var x=document.getElementsByClassName("english");
+	      var j;
+	      for (j = 0; j < x.length; j++) {
+	      x[j].style.display = "none";}
+
+	      var y = document.getElementsByClassName("bengali");
+	      var i;
+	      for (i = 0; i < y.length; i++) {
+	      y[i].style.display= "inherit";
+	      }}
+	      function LangEn(){
+	      var x=document.getElementsByClassName("bengali");
+	      var j;
+	      for (j = 0; j < x.length; j++) {
+	      x[j].style.display = "none";}
+
+	      var y = document.getElementsByClassName("english");
+	      var i;
+	      for (i = 0; i < y.length; i++) {
+	      y[i].style.display= "inherit";
+	      }}
