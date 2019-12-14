ed1fda7a1b6c75721a007dbecddff9d7b17ca979
subhrendu@iitg.ernet.in <subhrendu@iitg.ernet.in>
2019-12-14 19:24:57 +0530

    modified:   about.html 	modified:   index.html 	modified:   js/langChange.js 	modified:   venue.html

--- js/langChange.js
+++ js/langChange.js
@@ -1,22 +1,32 @@
-	      function LangBn(){
-	      var x=document.getElementsByClassName("english");
-	      var j;
-	      for (j = 0; j < x.length; j++) {
-	      x[j].style.display = "none";}
-
-	      var y = document.getElementsByClassName("bengali");
-	      var i;
-	      for (i = 0; i < y.length; i++) {
-	      y[i].style.display= "inherit";
-	      }}
-	      function LangEn(){
-	      var x=document.getElementsByClassName("bengali");
-	      var j;
-	      for (j = 0; j < x.length; j++) {
-	      x[j].style.display = "none";}
-
-	      var y = document.getElementsByClassName("english");
-	      var i;
-	      for (i = 0; i < y.length; i++) {
-	      y[i].style.display= "inherit";
-	      }}
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+////////////////////////////////////////////////////
+function LangBn(){
+  var x=document.getElementsByClassName("english");
+  $("body").fadeOut("slow");
+  for (var j = 0; j < x.length; j++) {
+  	x[j].style.display = "none";}
+  var y = document.getElementsByClassName("bengali");
+  sleep(900);
+  for (var i = 0; i < y.length; i++) {
+  	y[i].style.display= "inherit";}
+  $("body").fadeIn(500);
+}
+////////////////////////////////////////////////////
+function LangEn(){
+  var x=document.getElementsByClassName("bengali");
+  $("body").fadeOut("slow");
+  for (var j = 0; j < x.length; j++) {
+  	x[j].style.display = "none";}
+  var y = document.getElementsByClassName("english");
+  sleep(900);
+  for (var i = 0; i < y.length; i++) {
+  	y[i].style.display= "inherit";}
+  $("body").fadeIn(500);
+}
